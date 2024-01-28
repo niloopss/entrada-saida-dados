16436a30819b50091f08f84225a8ae611069e866
Nivea Lins de Albuquerque Lopes <144292197+niloopss@users.noreply.github.com>
2024-01-28 06:12:34 -0300

    teste1

---
+++ entradaSaida.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+
+public class entradaSaida {
+    public static void main(String[] args) {
+        String nome;
+        int idade;
+        float altura;
+        String estadocivil;
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Bem vindo(a), para começar com o atendimento precisamos de alguns dados.");
+
+        System.out.println("Digite seu nome: ");
+        nome = leia.next();
+
+        System.out.println("Digite sua idade: ");
+        idade = leia.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        altura = leia.nextFloat();
+
+        System.out.println("Solteito ou casado? ");
+        estadocivil = leia.next();
+
+        System.out.println("\nConfirme os seus dados: ");
+        System.out.println("Nome: "+nome);
+        System.out.println("Idade: "+idade);
+        System.out.println("Altura: "+altura);
+        System.out.println("Estado civil: "+estadocivil);
+
+        System.out.println("Seus dados estão corretos?(Sim/Não)");
+        String resposta = leia.next().toLowerCase();
+    }
+
+    }
+
